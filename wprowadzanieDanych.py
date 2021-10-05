dc8365c5b9050e85b6c24c4da11b920ddc2d1291
Michal Czarnik <michalczarnik@MacBook-Air-Michal.local>
2021-10-05 19:05:03 +0200

    lab 1

---
+++ wprowadzanieDanych.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+print('Enter your name, surname and date of birth.')
+x, y, z = [str(x) for x in input().split()]
+print('NAME: ' + x + ' SURNAME: ' + y +' DATE OF BIRTH: ' + z)
---
+++ zapisDanych.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+print('Set your code.')
+code = input()
+
+print('Enter code.')
+input = input()
+
+if (input == code):
+    print('Bravo!')
+else:
+    print('Wrong code')
